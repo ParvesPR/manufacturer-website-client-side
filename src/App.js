11e7de53b048a7208bb3f036b05f1106ce920b13
ParvesPR <parves2585@gmail.com>
2022-05-23 12:08:07 +0600

    parts component added

--- src/App.js
+++ src/App.js
@@ -5,6 +5,7 @@ import Purchase from './page/Home/Purchase';
 import Login from './page/Login/Login';
 import RequireAuth from './page/Login/RequireAuth';
 import SignUp from './page/Login/SignUp';
+import Parts from './page/Parts/Parts';
 import NavBar from './page/Shared/NavBar';
 
 function App() {
@@ -19,6 +20,7 @@ function App() {
             <Purchase></Purchase>
           </RequireAuth>
         }></Route>
+        <Route path='/parts' element={<Parts></Parts>}></Route>
         <Route path='/login' element={<Login></Login>}></Route>
         <Route path='/signup' element={<SignUp></SignUp>}></Route>
       </Routes>
---
+++ src/page/Parts/Parts.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import Products from '../Home/Products';
+
+const Parts = () => {
+    return (
+        <div>
+            <Products></Products>
+        </div>
+    );
+};
+
+export default Parts;
