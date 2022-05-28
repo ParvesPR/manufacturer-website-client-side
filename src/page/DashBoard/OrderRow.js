fa6e93f04193e78f73c22690cac14ca2183feada
ParvesPR <parves2585@gmail.com>
2022-05-28 10:34:51 +0600

    display transaction id in my orders

--- src/page/DashBoard/OrderRow.js
+++ src/page/DashBoard/OrderRow.js
@@ -11,10 +11,16 @@ const OrderRow = ({ order, index, setCancel }) => {
             <td>{email}</td>
             <td>
                 {(price && !order.paid) && <Link to={`/dashboard/payment/${_id}`}> <button className='btn btn-xs btn-success'>Pay</button></Link>}
-                {(price && order.paid) && <button className='btn btn-sm btn-success text-white'>Paid</button>}
+                {(price && order.paid) &&
+                    <button className='btn btn-sm btn-success text-white' disabled>Paid</button>}
             </td>
             <td>
-                <label onClick={() => setCancel(order)} htmlFor="delete-confirm" className="btn btn-xs btn-error text-white"> Delete</label>
+                {(price && !order.paid) &&
+                    <label onClick={() => setCancel(order)} htmlFor="delete-confirm" className="btn btn-xs btn-error text-white"> Delete</label>
+                }
+                {(price && order.paid) &&
+                    <p className='font-semibold text-slate-700'>Transaction Id: <span className='font-semibold text-red-700'>{order.transactionId}</span></p>
+                }
             </td>
         </tr>
     );
