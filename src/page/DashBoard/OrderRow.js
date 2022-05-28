8360d819e9a4ebf1bf41e6166592171d0d09cc93
ParvesPR <parves2585@gmail.com>
2022-05-28 10:22:14 +0600

    show paid button after complete payment

--- src/page/DashBoard/OrderRow.js
+++ src/page/DashBoard/OrderRow.js
@@ -11,7 +11,7 @@ const OrderRow = ({ order, index, setCancel }) => {
             <td>{email}</td>
             <td>
                 {(price && !order.paid) && <Link to={`/dashboard/payment/${_id}`}> <button className='btn btn-xs btn-success'>Pay</button></Link>}
-                {(price && order.paid) && <button className='btn btn-sm btn-success text-success'>Paid</button>}
+                {(price && order.paid) && <button className='btn btn-sm btn-success text-white'>Paid</button>}
             </td>
             <td>
                 <label onClick={() => setCancel(order)} htmlFor="delete-confirm" className="btn btn-xs btn-error text-white"> Delete</label>
